49fd890b4caf5adb35a44830eb8274310281f48f
Christopher Holmes <holmes.chris94@yahoo.com>
2020-06-16 22:10:34 -0400

    Homework startup complete

---
+++ Assets/javascript/app.js
@@ -0,0 +1,121 @@
+
+var questions = [
+    {
+        title: "Example Question 1:",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        answer: "answer from choices"
+    },
+    {
+        title: "Example Question 2:",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        answer: "answer from choices"
+    }
+];
+//Hook Elements from the HTML
+//Hook container element
+var containerEl = document.querySelector(".container");
+//Hook timer element
+var timerDisplay = document.querySelector(".timer");
+
+//Create dynamic elements
+//Create h1 to show starting heading
+var startText = document.createElement("h1");
+//Create button to start quiz
+var startBtn = document.createElement("button");
+//Creat P tag to display question
+var questionText = document.createElement("p");
+
+//Declare global variables
+//Variable to store timer number
+var timer = 75;
+//Variable to store current index
+var index = 0;
+
+//**FUNCTIONS**
+//Function that loads content when page first opens
+function openingPage() {
+    //Add text to Header
+    startText.textContent = "Welcome to the Quiz!";
+    //Add text to Button
+    startBtn.textContent = "Start Quiz"
+    //Append text to Container
+    containerEl.appendChild(startText);
+    //Append button to Container
+    containerEl.appendChild(startBtn);
+}
+//Function that shows the question and starts the timer
+function startQuiz() {
+    //Show timer funtion
+    showTimer();
+    //Call next question function
+    nextQuestion();
+}
+
+//Function that handles the timer
+function showTimer() {
+    //Display timer to screen
+    timerDisplay.textContent = timer;
+    //Create setInterval and store it to a variable
+    var timeInterval = setInterval(function () {
+        //Decrease timer by 1
+        timer--;
+        // Display timer to screen
+        timerDisplay.textContent = timer;
+        if (timer === 0) {
+            clearInterval(timeInterval)
+        }
+    }, 1000)
+
+
+}
+//Function that handles and displays the next question
+function nextQuestion() {
+    //Declare a variable  to store current question. Assign the current question
+    var currentQuestion = questions[index];
+    //Empty container element
+    containerEl.textContent = "";
+    //Add current question title to the question display variable
+    questionText.textContent = currentQuestion.title;
+    //Append the question display variable to the container
+    containerEl.appendChild(questionText);
+    //Create a div element to wrap the "choices"
+    var answersDiv = document.createElement("div");
+//Create a for loop
+for (let i = 0; i < currentQuestion.choices.length; i++) {
+    //Create button elements for each choice
+    var answerBtn = document.createElement("button");
+    //Add a class to each button to be used with the even listener
+    answerBtn.classList.add("choiceBtn");
+    //Add text to each button from question choices
+    answerBtn.textContent = currentQuestion.choices[i];
+    //Append buttons to the div element created to wrap choices
+    answersDiv.appendChild(answerBtn);
+    
+}
+
+//Append div element to the container
+containerEl.appendChild(answersDiv);
+
+};
+
+//Function to check the answer and display the following question
+function checkAnswer(event){
+    //If event.target.matches(--Choice button class--)
+    if(event.target.matches(".choiceBtn")){
+    //Logic to check for right answer
+    //Increase index by 1
+    index++;
+    //Show next question
+    nextQuestion()
+    
+}
+
+
+}
+
+//Add event listener to start quiz
+startBtn.addEventListener("click", startQuiz);
+//Add event listener to choice button
+document.addEventListener("click", checkAnswer);
+//Call function to show opening page
+openingPage()
